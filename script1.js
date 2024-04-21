99626e3d0d91183a1b063dd1fb5eed02239effe8
mahabub-alam <nishat.man2017@gmail.com>
2024-04-21 22:42:50 +0600

    3

--- script1.js
+++ script1.js
@@ -5,9 +5,11 @@ let buttonarray = Array.from(buttons);
 
 let s = ""
 screen.value = s;
+let fg = 0;
 
 buttonarray.forEach(element => {
     element.addEventListener('click', (e) => {
+
         let c = e.target.innerHTML;
         if (c == "DEL") {
             s = s.substring(0, s.length - 1);
@@ -17,8 +19,13 @@ buttonarray.forEach(element => {
         }
         else if (c == "=") {
             s = eval(s);
+            fg = 1;
         }
         else {
+            if (fg == 1) {
+                s = "";
+                fg = 0;
+            }
             s += e.target.innerHTML;
         }
         screen.value = s;
